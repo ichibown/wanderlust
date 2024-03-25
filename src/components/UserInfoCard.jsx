0bc7a4b06aee8cf00b2003f1daa5156d532d2c16
Xu Bowen <xubowen92@gmail.com>
2024-03-25 21:04:33 +0800

    fix: polish anim

--- src/components/UserInfoCard.jsx
+++ src/components/UserInfoCard.jsx
@@ -6,28 +6,29 @@ import Typography from '@mui/material/Typography';
 import { ExpandContext } from '../App';
 
 const UserTextContent = ({ name, motto, fontSize, center }) => {
+  const isExpanded = fontSize > 0;
   return (
     <Box sx={{
       display: 'flex',
       flexDirection: 'column',
       alignItems: center ? 'center' : 'start',
       marginLeft: center ? '0' : '12px',
-      opacity: fontSize > 0 ? 1 : 0,
-      transition: 'opacity 0.3s',
+      opacity: isExpanded ? 1 : 0,
+      transform: isExpanded ? 'translateY(0)' : (center ? 'translateY(-20px)' : 'translateY(20px)'),
+      transition: 'opacity 0.3s 0.3s, transform 0.3s 0.3s',
     }}  >
       <Typography color="text.primary"
         sx={{
           fontSize: fontSize,
           maxLines: 1,
-          transition: 'font-size 0.3s'
         }} >
         {name}
       </Typography>
       <Typography color="text.secondary"
         sx={{
           fontSize: fontSize * 0.75,
+          maxLines: 1,
           textAlign: 'center',
-          transition: 'font-size 0.3s'
         }} >
         {motto}
       </Typography>
@@ -43,7 +44,7 @@ const UserAvatarContent = ({ avatar, isExpanded }) => {
       sx={{
         width: isExpanded ? '72px' : '32px',
         height: isExpanded ? '72px' : '32px',
-        margin: isExpanded ? '8px 0 8px 0' : '0',
+        margin: isExpanded ? '8px 0 8px 72px' : '0',
         transition: 'width 0.3s, height 0.3s, margin 0.3s',
       }} />
   );
@@ -53,7 +54,12 @@ const UserInfoCard = ({ userInfo }) => {
   const expandState = useContext(ExpandContext);
   const isExpanded = expandState.isExpanded;
   return (
-    <Box sx={{ padding: '12px' }}>
+    <Box sx={{
+      padding: '12px',
+      maxWidth: isExpanded ? '280px' : '100%',
+      height: isExpanded ? '180px' : '64px',
+      transition: 'max-width 0.3s, height 0.3s',
+    }}>
       <Box sx={{
         display: 'flex',
         flexDirection: 'row',
@@ -63,21 +69,21 @@ const UserInfoCard = ({ userInfo }) => {
           maxWidth: '280px',
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
         }}>
-          <UserAvatarContent avatar={userInfo.avatar} isExpanded={isExpanded} />
+          <UserAvatarContent
+            avatar={userInfo.avatar}
+            isExpanded={isExpanded} />
           <UserTextContent
             name={userInfo.name}
             motto={userInfo.motto}
-            isExpanded={isExpanded}
             fontSize={isExpanded ? 20 : 0}
             center={true} />
         </Box>
         <UserTextContent
           name={userInfo.name}
           motto={userInfo.motto}
-          isExpanded={isExpanded}
-          fontSize={isExpanded ? 0 : 14} center={false} />
+          fontSize={isExpanded ? 0 : 14}
+          center={false} />
       </Box>
     </Box>
   );
