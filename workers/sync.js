243349aecf577f70bd990a569638114b01d69e37
Xu Bowen <xubowen@keep.com>
2024-03-27 11:29:11 +0800

    feat: add cron sync worker snippet

---
+++ workers/sync.js
@@ -0,0 +1,27 @@
+/**
+ * Use cron polling worker to do Strava data sync.
+ * 
+ * TOOD: Use https://developers.strava.com/docs/webhooks/ in the future.
+ */
+
+const BASE_URL = "YOUR_BASE_URL"
+const PASSWORD = "YOUR_PASSWORD"
+
+export default {
+
+  async fetch(request, env, ctx) {
+    return new Response('Hello World!');
+  },
+
+  async scheduled(event, env, ctx) {
+    ctx.waitUntil(
+      fetch(`${BASE_URL}/strava/sync`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-password': PASSWORD,
+        },
+      }).then(response => response.text().then(text => console.log(text)))
+    );
+  },
+};
